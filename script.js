5d9d0dc68e971d32808b19e8c1f9426a8a11ac19
Orlando Flores <orlando_flores@id.uff.br>
2021-04-24 19:09:45 -0300

    adiciona estilizacao, soma dos valores dos gastos e apaga o gasto quando clicado na tabela de gastos

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const inputExpense = document.getElementById('input-expense');
+const inputDescription = document.getElementById('input-description');
 const inputDate = document.getElementById('input-date');
 const inputValue = document.getElementById('input-value');
 const addBtn = document.getElementById('add-btn');
 const tBody = document.getElementsByTagName('tbody')[0];
 const inputs = document.querySelectorAll('input');
+const incomesCard = document.querySelector('#incomes');
+const expensesCard = document.querySelector('#expenses');
+const totalCard = document.querySelector('#total');
 
 const verifyInputs = () => {
   let result = true;
@@ -15,19 +18,65 @@ const verifyInputs = () => {
 
 const addExpense = () => {
   addBtn.addEventListener('click', () => {
-    if(!verifyInputs()) alert('Type all the infos');
+    if(!verifyInputs()) {
+      alert('Type all the infos');
+      return;
+    } 
     const tr = document.createElement('tr');
     inputs.forEach((input) => {
       const td = document.createElement('td');
-      td.innerText = input.value;
+      if (input.id === 'input-value') {
+        td.innerText = `${input.value} R$`;
+        input.value < 0 ? td.classList.add('expenses') : td.classList.add('incomes');
+      } else {
+        td.innerText = `${input.value}`;
+      }
       tr.appendChild(td);
     })
+    tr.addEventListener('click', (event) => deleteRow(event.currentTarget));
     tBody.appendChild(tr);
     inputs.forEach((input) => input.value = '');
+    updateIncomes();
+    updateExpenses();
+    updateTotal();
   })
 }
 
-// const updateIncomes
+const updateExpenses = () => {
+  const expenses = document.querySelectorAll('.expenses');
+  let sum = 0;
+  expenses.forEach((expense) => {
+    const value = parseFloat(expense.innerText.split(' ')[0])
+    sum += value;
+  });
+  expensesCard.innerText = `${sum.toFixed(2)} R$`;
+  return sum;
+}
+
+const updateIncomes = () => {
+  const incomes = document.querySelectorAll('.incomes');
+  let sum = 0;
+  incomes.forEach((income) => {
+    const value = parseFloat(income.innerText.split(' ')[0])
+    sum += value;
+  });
+  incomesCard.innerText = `${sum.toFixed(2)} R$`;
+  return sum;
+}
+
+const updateTotal = () => {
+  const income = updateIncomes();
+  const expense = updateExpenses();
+  const total = income + expense;
+  totalCard.innerText = `${total.toFixed(2)} R$`
+}
+
+const deleteRow = (event) => {
+  event.remove();
+  updateExpenses();
+  updateIncomes();
+  updateTotal();
+}
 
 window.onload = () => {
   addExpense();
