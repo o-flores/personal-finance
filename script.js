901e99a0d07be71e382c1b82c105a34fe13a6541
Orlando Flores <orlando_flores@id.uff.br>
2021-04-27 00:27:06 -0300

    adiciona algum estilo, salva as tr no local storage e começa a criar a funcao getLocalStorage qnd o mes e clicado no meu

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const incomesCard = document.querySelector('#incomes');
 const expensesCard = document.querySelector('#expenses');
 const totalCard = document.querySelector('#total');
 const months = document.querySelectorAll('#months li');
+const saveBtn = document.getElementById('save-btn');
 
 const verifyInputs = () => {
   let result = true;
@@ -20,7 +21,7 @@ const verifyInputs = () => {
 const createRow = (input) => {
   const td = document.createElement('td');
   if (input.id === 'input-value') {
-    td.innerText = `${input.value} R$`;
+    td.innerText = `${input.value}R$`;
     input.value < 0 ? td.classList.add('expenses') : td.classList.add('incomes');
   } else {
     td.innerText = `${input.value}`;
@@ -30,8 +31,13 @@ const createRow = (input) => {
 
 const addTransaction = () => {
   addBtn.addEventListener('click', () => {
+    const selected = document.querySelector('.selected');
     if (!verifyInputs()) {
       alert('Type all the infos');
+
+    }
+    if (!selected) {
+      alert('Selecione um mês');
       return;
     }
     const tr = document.createElement('tr');
@@ -84,20 +90,57 @@ const deleteRow = (event) => {
   event.remove();
   updateAll();
 }
-const removeSelected = () => {
+
+const setSelectedMonth = (event) => {
   const selected = document.querySelector('.selected');
-  if(selected) selected.classList.remove('selected');
+  const monthClass = event.target.classList;
+  if (!selected) monthClass.add('selected');
+  else {
+    if (monthClass.contains('selected')) monthClass.toggle('selected');
+  }
 }
+
 const monthsClicked = () => {
   months.forEach((month) => {
-    month.addEventListener('click', (event) => {
-      removeSelected();
-      event.target.classList.add('selected');
+    month.addEventListener('click', setSelectedMonth);
+    month.addEventListener('click', getLocalStorage);
+  });
+};
+
+const saveOnLocalStorage = () => {
+  saveBtn.addEventListener('click', () => {
+    const selected = document.querySelector('.selected');
+    const rows = document.querySelectorAll('tbody tr');
+    console.log(rows);
+    rows.forEach((row, index) => {
+      localStorage.setItem(`${selected.innerText}-${index}`, row.innerText);
     })
   });
+  // localStorage.setItem(`${month}`, JSON.stringify(row));
+  // console.log(JSON.parse(localStorage.getItem(`${month}`)));
+}
+
+const getLocalStorage = (event) => {
+  const month = event.target.innerText;
+  for (let index = 0; index < localStorage.length; index += 1) {
+    // console.log(localStorage.key(index).split('-')[0]);
+    if (localStorage.key(index).split('-')[0] === month) {
+      const row = document.createElement('tr');
+      const text = localStorage.getItem(localStorage.key(index));
+      const space = text.indexOf('$');
+      const date = text.slice(space + 2);
+      // const newText = text.split(' ');
+
+      // console.log(newText[0]);
+      // row.innerText = text;
+      // console.log(row);
+      // tBody.appendChild(row);
+    }
+  }
 }
 
 window.onload = () => {
   addTransaction();
   monthsClicked();
+  saveOnLocalStorage();
 }
